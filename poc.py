f8d651c73a86a6b14db31baa310b1f10e77b2cda
Jennifer Schweers <orange.jenny@gmail.com>
2020-10-07 18:11:42 -0400

    Added domain login and hit authenticated view (project dashboard)

--- poc.py
+++ poc.py
@@ -1,3 +1,4 @@
+import os
 from locust import HttpUser, between, task
 
 
@@ -9,7 +10,24 @@ class WebsiteUser(HttpUser):
     #host = "https://staging.commcarehq.org"
     host = "http://localhost:8000"
 
-    @task
-    def style(self):
-        response = self.client.get("/styleguide/")
+    domain = "bosco"
+
+    def on_start(self):
+        response = self.client.get(f'/a/{self.domain}/login/')   # get cookies
+        response = self.client.post(
+            f'/a/{self.domain}/login/',
+            {
+                "auth-username": os.environ['LOCUST_USERNAME'],
+                "auth-password": os.environ['LOCUST_PASSWORD'],
+                "cloud_care_login_view-current_step": ['auth'],     # fake out two_factor ManagementForm
+            },
+            headers={"X-CSRFToken": response.cookies.get('csrftoken')}
+        )
         assert(response.status_code == 200)
+        assert('Sign In' not in response.text)  # make sure we weren't just redirected back to login
+
+    @task
+    def dashboard(self):
+        response = self.client.get("/a/bosco/dashboard/project/")
+        assert(response.status_code == 200)
+        assert('fcc-reports' in response.text)
